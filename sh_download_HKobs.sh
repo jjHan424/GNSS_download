829093b3f1fe296f7f9cc61a443b5aef07f54c68
HanJunjie <HanJunjie@whu.edu.cn>
2022-03-27 17:08:37 +0800

    添加香港观测值下载&&gnssfile更新

--- sh_download_HKobs.sh
+++ sh_download_HKobs.sh
@@ -29,24 +29,36 @@ if ($#argv >= 3) then
   set count = $3
 endif
 
-set AC = grm
+set sample = 5
 if ($#argv >= 4) then
-  set AC = $4
+  set sample = $4
+endif
+
+set beg = 0
+set last = 6
+if ($#argv >= 6) then
+  set beg = $5
+  set last = $6
+endif
+
+set AC = grm
+if ($#argv >= 7) then
+  set AC = $7
 endif
 
 set ftppath = ftp://igs.gnsswhu.cn/pub
 set ftpAC = WHU
-if ($5 == 1) then
+if ($8 == 1) then
   set ftppath = https://cddis.nasa.gov/archive
   set ftpAC = CDDIS
-else if ($5 == 2) then
+else if ($8 == 2) then
   set ftppath = ftp://igs.gnsswhu.cn/pub
   set ftpAC = WHU
 endif
 
-set workdir = /home/hanjunjie/data/IONO/$yyyy
-if ($#argv >= 6) then
-  set workdir = $6
+set workdir = /home/hanjunjie/data/IONOGRID/$yyyy
+if ($#argv >= 9) then
+  set workdir = $9
 endif
 
 set yy = `echo $yyyy | awk '{printf("%2.2d",$1-int($1/100)*100)}'`
@@ -54,15 +66,16 @@ echo $yyyy $doy $count $AC $ftpAC $workdir
 
 set bindir = /home/hanjunjie/tools/zbin/bin
 set crx2rnx = /home/hanjunjie/tools/CRX2RNX
+set gfzrnx = /home/hanjunjie/tools/gfzrnx
 #set rec list for observation
 #set rec_list = (areg arev areq wtza wtzz irkj irkm suth sutv dgar dgav)
 #set rec_list = (yarr yar3)
 #set rec_list = (wtza wtzz yarr yar3)
 #set rec_list = (areg areq wtzz wtza yarr yar3 irkj irkm gold gol2 wtzr wtzs)
-set rec_list = (ascg)
+#set rec_list = (hkcl hkfn hkks hkkt hklm hklt hkmw hknp hkoh hkpc hksc hksl hkss hkst hktk hkws kyc1 t430)
+set rec_list = (hkqt)
 set ftpUPD1 = http://igmas.users.sgg.whu.edu.cn/products/download/directory/products/upd
 set ftpUPD2 = http://igmas.users.sgg.whu.edu.cn/products/download/directory/products/upd
-
 while($count)
   # date transform
   set rec_index = 1
@@ -72,6 +85,7 @@ while($count)
   set ymd_day = `echo $date | cut -b 3-4`
   set WEEKD  = `$bindir/mjday $doy $yyyy | awk '{nwk=int(($1-44244)/7);nwkd=$1-44244-nwk*7;print nwk*10+nwkd}'`
   set WEEK   = `echo $WEEKD | awk '{print substr($1,1,4)}'`
+  
   echo $yyyy-$mm-$ymd_day
   
   # set workdir
@@ -80,7 +94,8 @@ while($count)
   set dcbdir = $workdir/DCB
   set sp3dir = $workdir/SP3
   set clkdir = $workdir/CLK
-  set obsdir = $workdir/OBS
+  set obsdir = $workdir/OBS/$cdoy/${sample}S
+  echo "${obsdir}"
   set upddir = $workdir/UPD
   if (! -d $workdir) mkdir -p $workdir
   if (! -d $proddir) mkdir -p $proddir
@@ -90,8 +105,68 @@ while($count)
   if (! -d $clkdir) mkdir -p $clkdir
   if (! -d $upddir) mkdir -p $upddir
   if (! -d $obsdir) mkdir -p $obsdir
-  if (! -d $obsdir/$cdoy) mkdir -p $obsdir/$cdoy
   cd $workdir
+  # observation rinex3
+  while($#rec_list >= $rec_index)
+    set crt_last = $last
+    set crt_beg   = `echo $beg | awk '{printf("%2.2d\n",$1)}'`
+    set Merge = ""
+    set crt_rec = $rec_list[$rec_index]
+    set CRT_REC = `cat /home/hanjunjie/tools/download/site_list | grep $crt_rec | cut -b 6-14`
+    echo "Downloading $rec_list[$rec_index] obs from $ftpAC"
+    echo "samlpe:${sample}s  last:${crt_last}h"
+    if ($sample == 5) then
+        while(${crt_last} > 0)
+            set crt_beg   = `echo $crt_beg | awk '{printf("%2.2d\n",$1)}'`
+            echo "${crt_beg}h"
+            if ($crt_beg >= 23) then
+                set crt_last = 0
+            endif
+            curl -c .urs_cookies -b .urs_cookies -n -L "ftp://ftp.geodetic.gov.hk/rinex3/${yyyy}/${cdoy}/${crt_rec}/5s/${CRT_REC}_R_${yyyy}${cdoy}${crt_beg}00_01H_05S_MO.crx.gz" -O
+            gunzip -f ${CRT_REC}_R_${yyyy}${cdoy}${crt_beg}00_01H_05S_MO.crx.gz
+            mv ${CRT_REC}_R_${yyyy}${cdoy}${crt_beg}00_01H_05S_MO.crx $crt_rec${cdoy}${crt_beg}.${yy}d
+            ${crx2rnx} ${crt_rec}${cdoy}${crt_beg}.${yy}d
+            set Merge = "${Merge} ${crt_rec}${cdoy}${crt_beg}.${yy}o "
+            echo $Merge
+            @ crt_last --
+            @ crt_beg ++
+        end
+        ${gfzrnx} -finp ${Merge} -kv > ${crt_rec}${cdoy}all.${yy}o
+        mv ${crt_rec}${cdoy}all.${yy}o ${obsdir}/${crt_rec}${cdoy}0.${yy}o
+    endif
+
+    if ($sample == 1) then
+        while(${crt_last} > 0)
+            set crt_beg   = `echo $crt_beg | awk '{printf("%2.2d\n",$1)}'`
+            echo "${crt_beg}h"
+            if ($crt_beg >= 23) then
+                set crt_last = 0
+            endif
+            curl -c .urs_cookies -b .urs_cookies -n -L --silent "ftp://ftp.geodetic.gov.hk/rinex3/${yyyy}/${cdoy}/${crt_rec}/1s/${CRT_REC}_R_${yyyy}${cdoy}${crt_beg}00_01H_01S_MO.crx.gz" -O
+            gunzip -f ${CRT_REC}_R_${yyyy}${cdoy}${crt_beg}00_01H_01S_MO.crx.gz
+            mv ${CRT_REC}_R_${yyyy}${cdoy}${crt_beg}00_01H_01S_MO.crx $crt_rec${cdoy}${crt_beg}.${yy}d
+            ${crx2rnx} ${crt_rec}${cdoy}${crt_beg}.${yy}d
+            set Merge = "${Merge} ${crt_rec}${cdoy}${crt_beg}.${yy}o "
+            echo $Merge
+            @ crt_last --
+            @ crt_beg ++
+        end
+        ${gfzrnx} -finp ${Merge} -kv > ${crt_rec}${cdoy}all.${yy}o
+        mv ${crt_rec}${cdoy}all.${yy}o ${obsdir}/${crt_rec}${cdoy}0.${yy}o
+    endif
+    
+    if ($sample == 30) then
+          curl -c .urs_cookies -b .urs_cookies -n -L "ftp://ftp.geodetic.gov.hk/rinex3/${yyyy}/${cdoy}/${crt_rec}/30s/${CRT_REC}_R_${yyyy}${cdoy}0000_01D_30S_MO.crx.gz" -O
+          gunzip -f ${CRT_REC}_R_${yyyy}${cdoy}0000_01D_30S_MO.crx.gz
+          mv ${CRT_REC}_R_${yyyy}${cdoy}0${crt_beg}00_01D_30S_MO.crx $crt_rec${cdoy}0.${yy}d
+          ${crx2rnx} ${crt_rec}${cdoy}0.${yy}d
+          mv ${crt_rec}${cdoy}0.${yy}o ${obsdir}/${crt_rec}${cdoy}0.${yy}o
+    endif
+   
+    rm -rf *.${yy}o
+    rm -rf *.${yy}d
+    @ rec_index ++
+  end
   # download sp3 and clk
   echo "Downloading sp3 from $ftpAC"
   if (! -e igs$WEEKD.sp3.Z) curl -c .urs_cookies -b .urs_cookies -n -L --silent "$ftppath/gps/products/mgex/$WEEK/grm$WEEKD.sp3.Z" -O
@@ -138,32 +213,7 @@ while($count)
   cp /home/iGMAS/gnss_product/upd/${yyyy}/${cdoy}/upd_ewl_${yyyy}${cdoy}_GEC $upddir/
   
 
-  # observation rinex3 first
-  while($#rec_list >= $rec_index)
-    set crt_rec = $rec_list[$rec_index]
-    set CRT_REC = `cat /home/hanjunjie/tools/site_list | grep $crt_rec | cut -b 6-14`
-    echo "Downloading $rec_list[$rec_index] obs from $ftpAC"
-    curl -c .urs_cookies -b .urs_cookies -n -L --silent "$ftppath/gps/data/daily/$yyyy/$cdoy/${yy}d/${CRT_REC}_R_${yyyy}${cdoy}0000_01D_30S_MO.crx.gz" -O    
-    if (-f ${CRT_REC}_R_${yyyy}${cdoy}0000_01D_30S_MO.crx.gz) then
-      gunzip -f ${CRT_REC}_R_${yyyy}${cdoy}0000_01D_30S_MO.crx.gz
-      mv ${CRT_REC}_R_${yyyy}${cdoy}0000_01D_30S_MO.crx $crt_rec${cdoy}0.${yy}d
-    endif
-    if (! -f $crt_rec${cdoy}0.${yy}d) then
-      echo "Waring:No rinex3 observation $crt_rec"
-      curl -c .urs_cookies -b .urs_cookies -n -L --silent "$ftppath/gps/data/daily/$yyyy/$cdoy/${yy}d/$crt_rec${cdoy}0.${yy}d.Z" -O
-      if (! -e $crt_rec${cdoy}0.${yy}d.Z) then
-        echo "ERROR:No observation $crt_rec"
-      else
-        gunzip -f $crt_rec${cdoy}0.${yy}d.Z
-      endif     
-    endif
-    if (-f $crt_rec${cdoy}0.${yy}d) then
-      ${crx2rnx} $crt_rec${cdoy}0.${yy}d
-      mv $crt_rec${cdoy}0.${yy}o $obsdir/$cdoy/$crt_rec${cdoy}0.${yy}o
-      rm -rf $crt_rec${cdoy}0.${yy}d
-    endif
-    @ rec_index ++
-  end
+  
   @ doy ++
   @ count --
 
